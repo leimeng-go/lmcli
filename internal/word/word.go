df0f0368d6637bceeea4f9fa3f9e6c5bb9d782a8
leimeng <leimeng@qhgctech.com>
2021-08-08 21:54:24 +0800

    'updatecode'

---
+++ internal/word/word.go
@@ -0,0 +1,19 @@
+package word
+
+import "strings"
+
+func ToLower(s string)string{
+	return strings.ToLower(s)
+}
+func ToUpper(s string)string{
+	return strings.ToUpper(s)
+}
+func UnderscoreToUpperCamelCase(s string)string{
+	result:=""
+	array:=strings.Split(s, "_")
+	for i,v:=range array{
+		if i>0{
+
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"lmcli/cmd"
+	"log"
+)
+
 func main(){
-	
+	err:=cmd.Execute()
+	if err!=nil{
+		log.Fatalf("cmd.Excute err: %v",err)
+	}
 }
