0d88528bb157a412a9303376218c8916d0a872ff
leimeng <leimeng@qhgctech.com>
2021-09-09 23:47:22 +0800

    'updatecode'

--- internal/mongo2struct/mongo_test.go
+++ internal/mongo2struct/mongo_test.go
@@ -2,6 +2,7 @@ package mongo2struct
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 	"testing"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -17,11 +18,33 @@ func TestRunCommandEval(t *testing.T){
 		return
 	}
    client.Database("test")
-	result:=client.Database("test").RunCommand(ctx,bson.M{
-		"mapreduce": "xw_landlords",
-		 
-	})
-	t.Log(result.Err())
-	// t.Log(result.Err().Error())
+	testDB:=client.Database("test")
+	//result:=client.Database("test").RunCommand(ctx,bson.M{
+	//	"eval":"print(hello)",
+	//})
+	//t.Log(result.Err())
+	opts:=make([]*options.ListCollectionsOptions, 0)
+	opts=append(opts,options.ListCollections().SetNameOnly(true))
+
+	res,err:=testDB.ListCollections(ctx,bson.M{"name":"users"})
+	if err!=nil{
+		t.Error(err.Error())
+		return
+	}
+	defer res.Close(ctx)
+
+	for res.Next(ctx){
+		next:=&bsonx.Doc{}
+		err=res.Decode(next)
+		if err!=nil{
+			t.Error(err.Error())
+			return
+		}
+		elem,err:=next.LookupErr("va")
+	}
+
+	client.Database("test").ListCollections(ctx,bson.D{{"name","users"}}) {
+	}
+	// t.Log(result.Err().Error())j
 	// t.Log(client.Ping(ctx,nil))
 }
