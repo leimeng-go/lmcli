10ac2e1e73c6be79fd3d3825dd5288858f60ef04
meng lei <1955854667@qq.com>
2021-10-10 22:56:41 +0800

    mongo 元数据添加ObjectId类型

--- internal/sql2struct/mongometadata.go
+++ internal/sql2struct/mongometadata.go
@@ -2,16 +2,17 @@ package sql2struct
 
 var (
 	mongoToGoType = map[string]string{
-		"string": "string",
-		"double": "float64",
-		"bool":   "bool",
-		"Date":   "time.time",
-		"null":   "null",
-		"int":    "int32",
-		"object": "interface{}",
-		"long":   "int64",
-		"array":  "[]interface{}",
-		"date":   "time.Time",
+		"string":   "string",
+		"double":   "float64",
+		"bool":     "bool",
+		"Date":     "time.time",
+		"null":     "null",
+		"int":      "int32",
+		"object":   "interface{}",
+		"long":     "int64",
+		"array":    "[]interface{}",
+		"date":     "time.Time",
+		"objectId": "bson.ObjectId",
 	}
 )
 
